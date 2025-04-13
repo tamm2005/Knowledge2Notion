d403f89f3acbaaa9c5fe045687b221186f9ec070
tamm2005 <tamm2005@gmail.com>
2025-04-13 22:20:11 +0800

    Add page_content to fetch_linkedin_jobs return values and update LinkdinGrabService initialization

--- integrations/linkdin/linkdin.py
+++ integrations/linkdin/linkdin.py
@@ -6,7 +6,7 @@ from typing import List, Dict
 import json
 
 def fetch_linkedin_jobs(keywords: str = "DataEngineer", start: int=0, stop_sec:int=10, flag:int=1) -> List[Dict]:
-    notion_data,job_id_data,num_applicants_data = [], [], []
+    notion_data,job_id_data,num_applicants_data,page_content = [], [], [], []
     while start < 1000 and flag == 1:
         job_id_list = []
         print(start)
@@ -75,10 +75,22 @@ def fetch_linkedin_jobs(keywords: str = "DataEngineer", start: int=0, stop_sec:i
                     'num_applicants': {'rich_text': [{'text': {'content': num_applicants}}]}
                     }
                 )
+                page_content.append(
+                    {
+                        "text":contents, # pylint: disable=unsubscriptable-object
+                        'images':None, # pylint: disable=unsubscriptable-object
+                        'videos':None, # pylint: disable=unsubscriptable-object
+                        'pdf_links':None,
+                        'caption_strings':None,
+                        'web_bookmarks':None,
+                        'image_messages':None,
+                        'video_messages':None
+                    }
+                )
 
                 job_id_data.append(job_id)
                 num_applicants_data.append(num_applicants)
 
             start += len(page_jobs)
             time.sleep(stop_sec)  # Sleep for 1 second to avoid hitting the server too hard
-    return notion_data, job_id_data, num_applicants_data
+    return notion_data, job_id_data, num_applicants_data, page_content
--- services/linkdin.py
+++ services/linkdin.py
@@ -19,7 +19,7 @@ class LinkdinGrabService(object):
         self.keywords = keywords
         self.start = start
         self.stop_sec = stop_sec
-        self.job_description_list, self.job_id_data, self.num_applicants = fetch_linkedin_jobs(self.keywords,
+        self.job_description_list, self.job_id_data, self.num_applicants, self.page_content = fetch_linkedin_jobs(self.keywords,
                                  self.start,
                                  self.stop_sec)
 
@@ -29,18 +29,31 @@ class LinkdinGrabService(object):
                                     self.notion_token,
                                     self.databaseid,
                                     notion_data,
-                                    {}
+                                    self.page_content
                                 )
             job_id = self.job_id_data[index]
             status, notion_resp = notion_service.read_notion_response(job_id, "job_id")
             results = notion_resp.get("results", [])
+            
             if not results:
                 print(f"[NEW] job_id {job_id} not found in Notion.")
                 status_code,response_content = notion_service.write_to_notion_page()
+                
+                if status_code == 200:
+                    print("Page successfully Create to Notion!")
+                    page_id = response_content['id']
+                    for key, _ in self.page_content.items():
+                        notion_service.write_to_notion_content(page_id,key,key)
+                else:
+                    page_id = None
+                    print(response_content)
+                    print("Error uploading JSON data to Notion. Status code:", status_code)
+
                 print(status_code)
                 print(notion_data)
                 print(json.dumps(response_content, ensure_ascii=False, indent=2))
                 continue  # Or insert logic
+
             page_id = results[0]["id"]
             rich_texts = results[0]["properties"]["num_applicants"]["rich_text"][0]["text"]["content"]
             if self.num_applicants[index] != rich_texts:
