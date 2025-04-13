2a41786d3c9bccbbc69370a4aca1bcf17370f983
tamm2005 <tamm2005@gmail.com>
2025-04-14 02:15:09 +0800

    Refactor page_content structure to nest job post details under a "Post" key

--- integrations/linkdin/linkdin.py
+++ integrations/linkdin/linkdin.py
@@ -76,14 +76,16 @@ def fetch_linkedin_jobs(keywords: str = "DataEngineer", start: int=0, stop_sec:i
                 )
                 page_content.append(
                     {
-                        "text":[contents], # pylint: disable=unsubscriptable-object
-                        'images':[None], # pylint: disable=unsubscriptable-object
-                        'videos':[None], # pylint: disable=unsubscriptable-object
-                        'pdf_links':None,
-                        'caption_strings':None,
-                        'web_bookmarks':None,
-                        'image_messages':None,
-                        'video_messages':None
+                        "Post": {
+                            "text":[contents], # pylint: disable=unsubscriptable-object
+                            'images':[None], # pylint: disable=unsubscriptable-object
+                            'videos':[None], # pylint: disable=unsubscriptable-object
+                            'pdf_links':None,
+                            'caption_strings':None,
+                            'web_bookmarks':None,
+                            'image_messages':None,
+                            'video_messages':None
+                        }
                     }
                 )
 
